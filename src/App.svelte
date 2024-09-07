c11c8707bea6b750a59ab5e0e6101a826c348d00
Aiden DiPaolo <aidendipaolo@gmail.com>
2024-09-07 17:54:19 -0300

    Styled site and added more synths.

--- src/App.svelte
+++ src/App.svelte
@@ -5,7 +5,11 @@
   let beat = 0;
   let isPlaying = false;
 
-  let synths = [
+  ////////////////////////////////////////////////
+  //Synths
+  ////////////////////////////////////////////////
+
+  const basesynths = [
     new Tone.Synth().toDestination(),
     new Tone.Synth().toDestination(),
     new Tone.Synth().toDestination(),
@@ -15,6 +19,76 @@
     new Tone.Synth().toDestination(),
     new Tone.Synth().toDestination()
   ];
+
+  const monosynths = [
+    new Tone.MonoSynth().toDestination(),
+    new Tone.MonoSynth().toDestination(),
+    new Tone.MonoSynth().toDestination(),
+    new Tone.MonoSynth().toDestination(),
+    new Tone.MonoSynth().toDestination(),
+    new Tone.MonoSynth().toDestination(),
+    new Tone.MonoSynth().toDestination(),
+    new Tone.MonoSynth().toDestination()
+  ];
+
+  const fmsynths = [
+    new Tone.FMSynth().toDestination(),
+    new Tone.FMSynth().toDestination(),
+    new Tone.FMSynth().toDestination(),
+    new Tone.FMSynth().toDestination(),
+    new Tone.FMSynth().toDestination(),
+    new Tone.FMSynth().toDestination(),
+    new Tone.FMSynth().toDestination(),
+    new Tone.FMSynth().toDestination()
+  ];
+
+  const amsynths = [
+    new Tone.AMSynth().toDestination(),
+    new Tone.AMSynth().toDestination(),
+    new Tone.AMSynth().toDestination(),
+    new Tone.AMSynth().toDestination(),
+    new Tone.AMSynth().toDestination(),
+    new Tone.AMSynth().toDestination(),
+    new Tone.AMSynth().toDestination(),
+    new Tone.AMSynth().toDestination()
+  ];
+
+  const metalsynths = [
+    new Tone.MetalSynth().toDestination(),
+    new Tone.MetalSynth().toDestination(),
+    new Tone.MetalSynth().toDestination(),
+    new Tone.MetalSynth().toDestination(),
+    new Tone.MetalSynth().toDestination(),
+    new Tone.MetalSynth().toDestination(),
+    new Tone.MetalSynth().toDestination(),
+    new Tone.MetalSynth().toDestination()
+  ];
+
+  const plucksynths = [
+    new Tone.PluckSynth().toDestination(),
+    new Tone.PluckSynth().toDestination(),
+    new Tone.PluckSynth().toDestination(),
+    new Tone.PluckSynth().toDestination(),
+    new Tone.PluckSynth().toDestination(),
+    new Tone.PluckSynth().toDestination(),
+    new Tone.PluckSynth().toDestination(),
+    new Tone.PluckSynth().toDestination()
+  ];
+  
+  const instruments = [
+    basesynths,
+    monosynths,
+    fmsynths,
+    amsynths,
+    metalsynths,
+    plucksynths
+  ];
+
+  let synths = instruments[0];
+
+  ////////////////////////////////////////////////
+  //Scales
+  ////////////////////////////////////////////////
 
   const cmajor = ["C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5"];
   const dmajor = ["D4", "E4", "F#4", "G4", "A4", "B4", "C#5", "D5"];
@@ -35,6 +109,10 @@
   ];
 
   let scaleIndex = 0;
+
+  ////////////////////////////////////////////////
+  //Row Init and Functions
+  ////////////////////////////////////////////////
 
   let rows = [
     Array.from({length: 16}, (_, i) => ({note: scales[scaleIndex][7], active: false})),
@@ -92,61 +170,91 @@
       Array.from({length: 16}, (_, i) => ({note: scales[scaleIndex][0], active: false})) 
     ];
     
-    /* want the buttons to keep their active/inactive state when changing scales:
-    for(let i = 0; i < rows.length; i++) {
-      for(let j = 0; j < rows.length[0]; j++) {
-        rows[i][j].note = scales[scaleIndex][7-j];
-      }
-    }
-    */
+    // TODO: Need buttons to save their activity state when changing scales
+  }
+
+  const handleSynthChange = (synthIndex) => {
+    synths = instruments[synthIndex];
   }
 
 </script>
 
+<svelte:head>
+  <title> Sveltequencer </title>
+  <style>
+    @import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap');
+  </style>
+</svelte:head>
 
-<div class = "bpm-controls">
-  <label for = "bpm">{bpm} BPM</label>
-  <input type = "range" id = "bpm" min = "60" max = "240" bind:value = {bpm}/>
+<div class = "label">
+  <h> Sveltequencer </h>
 </div>
 
-<div class = "sequencer">
-  {#each beatIndicators as beatIndicator, bi}
-    <div class="beat-indicator {bi === beat - 1 ? 'live' : ''}"></div>
-  {/each}
+<div class = "content">
+  <div class = "bpm-controls">
+    <label for = "bpm">{bpm} BPM</label>
+    <input type = "range" id = "bpm" min = "60" max = "240" bind:value = {bpm}/>
+  </div>
 
-  {#each rows as row, i}
-    {#each row as note, j}
-      <button on:click = {() => handleNoteClick(i, j)} class = "note {note.active ? 'active' : ''}"></button>
-    {/each}  
-  {/each}
-</div>
+  <div class = "sequencer">
+    {#each beatIndicators as beatIndicator, bi} 
+      <div class="beat-indicator {bi === beat - 1 ? 'live' : ''}"></div>
+    {/each}
 
-<div class = "buttons"> 
-  {#if isPlaying}
-    <button on:click = {handleStopClick}>Stop</button>
+    {#each rows as row, i}
+      {#each row as note, j}
+        <button on:click = {() => handleNoteClick(i, j)} class = "note {note.active ? 'active' : ''}"></button>
+      {/each}  
+    {/each}
+  </div>
+
+  <div class = "buttons"> 
+    {#if isPlaying}
+      <button on:click = {handleStopClick} class = "playbtn">Stop</button>
     {:else}
-      <button on:click = {handlePlayClick}>Play</button>
+      <button on:click = {handlePlayClick} class = "playbtn">Play</button>
     {/if}
-</div>
+  </div>
 
-<div class = "dropdown">
-  <button class = "dropbtn">Scales</button>
-  <div class = "dropdown-content">
-    <button on:click = {() => handleScaleChange(0)} class = "cmajorbtn">C-Major</button>
-    <button on:click = {() => handleScaleChange(1)} class = "dmajorbtn">D-Major</button>
-    <button on:click = {() => handleScaleChange(2)} class = "emajorbtn">E-Major</button>
-    <button on:click = {() => handleScaleChange(3)} class = "fmajorbtn">F-Major</button>
-    <button on:click = {() => handleScaleChange(4)} class = "gmajorbtn">G-Major</button>
-    <button on:click = {() => handleScaleChange(5)} class = "amajorbtn">A-Major</button>
-    <button on:click = {() => handleScaleChange(6)} class = "bmajorbtn">B-Major</button>
+  <div class = "dropdown">
+    <button class = "dropbtn">Scales</button>
+    <div class = "dropdown-content">
+      <button on:click = {() => handleScaleChange(0)} class = "cmajorbtn">C-major</button>
+      <button on:click = {() => handleScaleChange(1)} class = "dmajorbtn">D-major</button>
+      <button on:click = {() => handleScaleChange(2)} class = "emajorbtn">E-major</button>
+      <button on:click = {() => handleScaleChange(3)} class = "fmajorbtn">F-major</button>
+      <button on:click = {() => handleScaleChange(4)} class = "gmajorbtn">G-major</button>
+      <button on:click = {() => handleScaleChange(5)} class = "amajorbtn">A-major</button>
+      <button on:click = {() => handleScaleChange(6)} class = "bmajorbtn">B-major</button>
+    </div>
+  </div>
+
+  <div class = "dropdown">
+    <button class = "dropbtn">Synths</button>
+    <div class = "dropdown-content">
+      <button on:click = {() => handleSynthChange(0)} class = "basesynthbtn">Normal Synth</button>
+      <button on:click = {() => handleSynthChange(1)} class = "monosynthbtn">Mono Synth</button>
+      <button on:click = {() => handleSynthChange(2)} class = "fmsynthbtn">FM Synth</button>
+      <button on:click = {() => handleSynthChange(3)} class = "amsynthbtn">AM Synth</button>
+      <button on:click = {() => handleSynthChange(4)} class = "metalsynthbtn">Metal Synth</button>
+      <button on:click = {() => handleSynthChange(5)} class = "plucksynthbtn">Pluck Synth</button>
+    </div>
   </div>
 </div>
 
+
 <style>
+  .label { 
+    position: absolute;
+    font-family: "press start 2p", system-ui;
+    font-weight: 400;
+    font-style: normal;
+  }
+
   .sequencer {
     display: grid;
     grid-template-columns: repeat(16, 1fr);
-    grid-gap: 3px;
+    grid-gap: 5px;
     width: 100%;
   }
 
@@ -155,18 +263,18 @@
     width: 50px;
     height: 50px;
     border: 1px solid #ccc;
-    border-radius: 2px;
     display: flex;
+    border-radius: 0px;
     justify-content: center;
     align-items: center;
-    font-size: 2rem;
+    font-size: 1rem;
   }
 
   .note.active {
     background: black;
     border: 1px solid black;
     color: black;
-    border-radius: 2px;
+    border-radius: 0px;
   }
 
   .bpm-controls {
@@ -175,22 +283,34 @@
 
   .bpm-controls label {
     color: white;
+    font-family: "press start 2p", system-ui;
+    font-weight: 400;
+    font-style: normal;
+    font-size: 13.5px;
   }
 
   .buttons {
     margin-top: 10px;
   }
+
+  .playbtn {
+    border-radius: 0px;
+    font-family: "press start 2p", system-ui;
+    font-weight: 400;
+    font-style: normal;
+    font-size: 14px;
+  }
   
  .beat-indicator {
     width: 10px;
     height: 10px;
-    border-radius: 50%;
+    border-radius: 0px;
     background: #555;
     display: flex;
     justify-content: center;
     align-items: center;
     color: #fff;
-    font-size: 1.5rem;
+    font-size: 0.5rem;
     margin: 0 auto;
   }
 
@@ -201,6 +321,14 @@
   .dropdown {
     margin-top: 5px;
     display: inline-block;
+  }
+
+  .dropbtn {
+    border-radius: 0px;
+    font-family: "press start 2p", system-ui;
+    font-weight: 400;
+    font-style: normal;
+    font-size: 14px;
   }
 
   .dropdown-content {
