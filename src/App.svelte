fd111be628b221bd92315961083017dcb91dbb4e
Aiden DiPaolo <aidendipaolo@gmail.com>
2024-09-17 19:52:55 -0300

    Refactored some code, cleaned up nav-bar + scales.

--- src/App.svelte
+++ src/App.svelte
@@ -1,118 +1,13 @@
 <script>
   import * as Tone from "tone";
+  import { instruments } from "./lib/synths.js";
+  import { scales } from "./lib/scales.js";
 
   let bpm = 120;
   let beat = 0;
   let isPlaying = false;
-
-  ////////////////////////////////////////////////
-  //Synths
-  ////////////////////////////////////////////////
-
-  const basesynths = [
-    new Tone.Synth().toDestination(),
-    new Tone.Synth().toDestination(),
-    new Tone.Synth().toDestination(),
-    new Tone.Synth().toDestination(),
-    new Tone.Synth().toDestination(),
-    new Tone.Synth().toDestination(),
-    new Tone.Synth().toDestination(),
-    new Tone.Synth().toDestination()
-  ];
-
-  const monosynths = [
-    new Tone.MonoSynth().toDestination(),
-    new Tone.MonoSynth().toDestination(),
-    new Tone.MonoSynth().toDestination(),
-    new Tone.MonoSynth().toDestination(),
-    new Tone.MonoSynth().toDestination(),
-    new Tone.MonoSynth().toDestination(),
-    new Tone.MonoSynth().toDestination(),
-    new Tone.MonoSynth().toDestination()
-  ];
-
-  const fmsynths = [
-    new Tone.FMSynth().toDestination(),
-    new Tone.FMSynth().toDestination(),
-    new Tone.FMSynth().toDestination(),
-    new Tone.FMSynth().toDestination(),
-    new Tone.FMSynth().toDestination(),
-    new Tone.FMSynth().toDestination(),
-    new Tone.FMSynth().toDestination(),
-    new Tone.FMSynth().toDestination()
-  ];
-
-  const amsynths = [
-    new Tone.AMSynth().toDestination(),
-    new Tone.AMSynth().toDestination(),
-    new Tone.AMSynth().toDestination(),
-    new Tone.AMSynth().toDestination(),
-    new Tone.AMSynth().toDestination(),
-    new Tone.AMSynth().toDestination(),
-    new Tone.AMSynth().toDestination(),
-    new Tone.AMSynth().toDestination()
-  ];
-
-  const metalsynths = [
-    new Tone.MetalSynth().toDestination(),
-    new Tone.MetalSynth().toDestination(),
-    new Tone.MetalSynth().toDestination(),
-    new Tone.MetalSynth().toDestination(),
-    new Tone.MetalSynth().toDestination(),
-    new Tone.MetalSynth().toDestination(),
-    new Tone.MetalSynth().toDestination(),
-    new Tone.MetalSynth().toDestination()
-  ];
-
-  const plucksynths = [
-    new Tone.PluckSynth().toDestination(),
-    new Tone.PluckSynth().toDestination(),
-    new Tone.PluckSynth().toDestination(),
-    new Tone.PluckSynth().toDestination(),
-    new Tone.PluckSynth().toDestination(),
-    new Tone.PluckSynth().toDestination(),
-    new Tone.PluckSynth().toDestination(),
-    new Tone.PluckSynth().toDestination()
-  ];
-  
-  const instruments = [
-    basesynths,
-    monosynths,
-    fmsynths,
-    amsynths,
-    metalsynths,
-    plucksynths
-  ];
-
   let synths = instruments[0];
-
-  ////////////////////////////////////////////////
-  //Scales
-  ////////////////////////////////////////////////
-
-  const cmajor = ["C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5"];
-  const dmajor = ["D4", "E4", "F#4", "G4", "A4", "B4", "C#5", "D5"];
-  const emajor = ["E4", "F#4,", "G#4", "A4", "B4", "C#5", "D#5", "E5"];
-  const fmajor = ["F4", "G4", "A4", "Bb4", "C5", "D5", "E5", "F5"];
-  const gmajor = ["G4", "A4", "B4", "C5", "D5", "E5", "F#5", "G5"];
-  const amajor = ["A4", "B4", "C#5", "D5", "E5", "F#5", "G#5", "A5"];
-  const bmajor = ["B4", "C#5", "D#5", "E5", "F#5", "G#5", "A#5", "B5"];
-  
-  const scales = [
-    cmajor,
-    dmajor,
-    emajor,
-    fmajor,
-    gmajor,
-    amajor,
-    bmajor
-  ];
-
   let scaleIndex = 0;
-
-  ////////////////////////////////////////////////
-  //Row Init and Functions
-  ////////////////////////////////////////////////
 
   let rows = [
     Array.from({length: 16}, (_, i) => ({note: scales[scaleIndex][7], active: false})),
@@ -148,6 +43,20 @@
     isPlaying = true; 
   };
 
+  const handleResetClick = () => {
+    rows = [
+      Array.from({length: 16}, (_, i) => ({note: scales[scaleIndex][7], active: false})),
+      Array.from({length: 16}, (_, i) => ({note: scales[scaleIndex][6], active: false})),
+      Array.from({length: 16}, (_, i) => ({note: scales[scaleIndex][5], active: false})),
+      Array.from({length: 16}, (_, i) => ({note: scales[scaleIndex][4], active: false})),
+      Array.from({length: 16}, (_, i) => ({note: scales[scaleIndex][3], active: false})),
+      Array.from({length: 16}, (_, i) => ({note: scales[scaleIndex][2], active: false})),
+      Array.from({length: 16}, (_, i) => ({note: scales[scaleIndex][1], active: false})),
+      Array.from({length: 16}, (_, i) => ({note: scales[scaleIndex][0], active: false}))
+    ];
+    beat = 0;
+  }
+
   const handleStopClick = () => {
     Tone.Transport.stop();
     isPlaying = false;
@@ -158,25 +67,16 @@
   }
 
   const handleScaleChange = (scaleIndex) => {
-
-    rows = [
-      Array.from({length: 16}, (_, i) => ({note: scales[scaleIndex][7], active: false})),
-      Array.from({length: 16}, (_, i) => ({note: scales[scaleIndex][6], active: false})),
-      Array.from({length: 16}, (_, i) => ({note: scales[scaleIndex][5], active: false})),
-      Array.from({length: 16}, (_, i) => ({note: scales[scaleIndex][4], active: false})),
-      Array.from({length: 16}, (_, i) => ({note: scales[scaleIndex][3], active: false})),
-      Array.from({length: 16}, (_, i) => ({note: scales[scaleIndex][2], active: false})),
-      Array.from({length: 16}, (_, i) => ({note: scales[scaleIndex][1], active: false})),
-      Array.from({length: 16}, (_, i) => ({note: scales[scaleIndex][0], active: false})) 
-    ];
-    
-    // TODO: Need buttons to save their activity state when changing scales
+    for(let i = 0, j = 7; i < rows.length; i++, j--) {
+      for(let obj of rows[i]) {
+        obj.note = scales[scaleIndex][j];
+      }
+    }
   }
 
   const handleSynthChange = (synthIndex) => {
     synths = instruments[synthIndex];
   }
-
 </script>
 
 <svelte:head>
@@ -186,66 +86,63 @@
   </style>
 </svelte:head>
 
-<div class = "nav">
-  <nav class = "navbar">
-    <ul>
-      <li>
-        <div class = "label">
-          <h> Sveltequencer </h>
-        </div>
-      </li>
-      <li>
-        <a class = "scales" href="#">Scales</a>
-        <ul>
-          <li><button on:click = {() => handleScaleChange(0)} class = "cmajorbtn">C-major</button></li>
-          <li><button on:click = {() => handleScaleChange(1)} class = "dmajorbtn">D-major</button></li>
-          <li><button on:click = {() => handleScaleChange(2)} class = "emajorbtn">E-major</button></li>
-          <li><button on:click = {() => handleScaleChange(3)} class = "fmajorbtn">F-major</button></li>
-          <li><button on:click = {() => handleScaleChange(4)} class = "gmajorbtn">G-major</button></li>
-          <li><button on:click = {() => handleScaleChange(5)} class = "amajorbtn">A-major</button></li>
-          <li><button on:click = {() => handleScaleChange(6)} class = "bmajorbtn">B-major</button></li>
-        </ul>
-      </li>
-      <li>
-        <a class = "synths" href="#">Synths</a>
-        <ul>
-          <li><button on:click = {() => handleSynthChange(0)} class = "basesynthbtn">Normal Synth</button></li>
-          <li><button on:click = {() => handleSynthChange(1)} class = "monosynthbtn">Mono Synth</button></li>
-          <li><button on:click = {() => handleSynthChange(2)} class = "fmsynthbtn">FM Synth</button></li>
-          <li><button on:click = {() => handleSynthChange(3)} class = "amsynthbtn">AM Synth</button></li>
-          <li><button on:click = {() => handleSynthChange(4)} class = "metalsynthbtn">Metal Synth</button></li>
-          <li><button on:click = {() => handleSynthChange(5)} class = "plucksynthbtn">Pluck Synth</button></li>
-        </ul>
-      </li>
-    </ul>
-  </nav>
+<nav class = "navbar">
+  <ul>
+    <li>
+      <div class = "label">
+        <h> Sveltequencer </h>
+      </div>
+    </li>
+    <li>
+      <a class = "scales" href="#">Scales</a>
+      <ul>
+        <li><button on:click = {() => handleScaleChange(0)} class = "cmajorbtn">C-major</button></li>
+        <li><button on:click = {() => handleScaleChange(1)} class = "dmajorbtn">D-major</button></li>
+        <li><button on:click = {() => handleScaleChange(2)} class = "emajorbtn">E-major</button></li>
+        <li><button on:click = {() => handleScaleChange(3)} class = "fmajorbtn">F-major</button></li>
+        <li><button on:click = {() => handleScaleChange(4)} class = "gmajorbtn">G-major</button></li>
+        <li><button on:click = {() => handleScaleChange(5)} class = "amajorbtn">A-major</button></li>
+        <li><button on:click = {() => handleScaleChange(6)} class = "bmajorbtn">B-major</button></li>
+      </ul>
+    </li>
+    <li>
+      <a class = "synths" href="#">Synths</a>
+      <ul>
+        <li><button on:click = {() => handleSynthChange(0)} class = "basesynthbtn">Normal Synth</button></li>
+        <li><button on:click = {() => handleSynthChange(1)} class = "monosynthbtn">Mono Synth</button></li>
+        <li><button on:click = {() => handleSynthChange(2)} class = "fmsynthbtn">FM Synth</button></li>
+        <li><button on:click = {() => handleSynthChange(3)} class = "amsynthbtn">AM Synth</button></li>
+        <li><button on:click = {() => handleSynthChange(4)} class = "metalsynthbtn">Metal Synth</button></li>
+        <li><button on:click = {() => handleSynthChange(5)} class = "plucksynthbtn">Pluck Synth</button></li>
+      </ul>
+    </li>
+  </ul>
+</nav>
+
+<div class = "bpm-controls">
+  <label for = "bpm">{bpm} BPM</label>
+  <input class = "bpm-slider" type = "range" id = "bpm" min = "60" max = "240" bind:value = {bpm}/>
 </div>
 
-<div class = "content">
-  <div class = "bpm-controls">
-    <label for = "bpm">{bpm} BPM</label>
-    <input type = "range" id = "bpm" min = "60" max = "240" bind:value = {bpm}/>
-  </div>
+<div class = "sequencer">
+  {#each beatIndicators as beatIndicator, bi} 
+    <div class="beat-indicator {bi === beat ? 'live' : ''}"></div>
+  {/each}
 
-  <div class = "sequencer">
-    {#each beatIndicators as beatIndicator, bi} 
-      <div class="beat-indicator {bi === beat - 1 ? 'live' : ''}"></div>
-    {/each}
+  {#each rows as row, i}
+    {#each row as note, j}
+      <button on:click = {() => handleNoteClick(i, j)} class = "note {note.active ? 'active' : ''}"></button>
+    {/each}  
+  {/each}
+</div>
 
-    {#each rows as row, i}
-      {#each row as note, j}
-        <button on:click = {() => handleNoteClick(i, j)} class = "note {note.active ? 'active' : ''}"></button>
-      {/each}  
-    {/each}
-  </div>
-
-  <div class = "buttons"> 
-    {#if isPlaying}
-      <button on:click = {handleStopClick} class = "playbtn">Stop</button>
-    {:else}
-      <button on:click = {handlePlayClick} class = "playbtn">Play</button>
-    {/if}
-  </div>
+<div class = "buttons"> 
+  {#if isPlaying}
+    <button on:click = {handleStopClick} class = "playbtn">Stop</button>
+  {:else}
+    <button on:click = {handlePlayClick} class = "playbtn">Play</button>
+  {/if}
+  <button on:click = {handleResetClick} class = "resetbtn">Reset</button>
 </div>
 
 
@@ -359,11 +256,15 @@
     font-size: 13.5px;
   }
 
+  .bpm-controls input {
+    width: 240px;
+  }
+
   .buttons {
     margin-top: 10px;
   }
 
-  .playbtn {
+  .buttons button {
     font-family: "press start 2p", system-ui;
     font-weight: 400;
     font-style: normal;
@@ -383,6 +284,6 @@
   }
 
   .live {
-    background: #05f18f;
+    background: #ff0000;
   } 
 </style>
