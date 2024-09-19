eee86cc45a1ce5b5dca8662ec84a10281f0825c6
Aiden DiPaolo <aidendipaolo@gmail.com>
2024-09-19 00:11:00 -0300

    Added minor scales.

--- src/lib/scales.js
+++ src/lib/scales.js
@@ -1,3 +1,5 @@
+// Major Scales
+
 const cmajor = ["C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5"];
 const dmajor = ["D4", "E4", "F#4", "G4", "A4", "B4", "C#5", "D5"];
 const emajor = ["E4", "F#4,", "G#4", "A4", "B4", "C#5", "D#5", "E5"];
@@ -5,7 +7,18 @@ const fmajor = ["F4", "G4", "A4", "Bb4", "C5", "D5", "E5", "F5"];
 const gmajor = ["G4", "A4", "B4", "C5", "D5", "E5", "F#5", "G5"];
 const amajor = ["A4", "B4", "C#5", "D5", "E5", "F#5", "G#5", "A5"];
 const bmajor = ["B4", "C#5", "D#5", "E5", "F#5", "G#5", "A#5", "B5"];
-  
+
+
+// Minor Scales
+
+const cminor = ["C4", "D4", "Eb4", "F4", "G4", "Ab4", "Bb4", "C5"];
+const dminor = ["D4", "E4", "F4", "G4", "A4", "Bb4", "C5", "D5"];
+const eminor = ["E4", "F#4,", "G4", "A4", "B4", "C5", "D5", "E5"];
+const fminor = ["F4", "G4", "Ab4", "Bb4", "C5", "Db5", "Eb5", "F5"];
+const gminor = ["G4", "A4", "Bb4", "C5", "D5", "Eb5", "F5", "G5"];
+const aminor = ["A4", "B4", "C5", "D5", "E5", "F5", "G5", "A5"];
+const bminor = ["B4", "C#5", "D5", "E5", "F#5", "G5", "A5", "B5"];
+
 export const scales = [
     cmajor,
     dmajor,
@@ -13,5 +26,12 @@ export const scales = [
     fmajor,
     gmajor,
     amajor,
-    bmajor
+    bmajor,
+    cminor,
+    dminor,
+    eminor,
+    fminor,
+    gminor,
+    aminor,
+    bminor
 ];
